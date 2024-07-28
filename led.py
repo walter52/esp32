655a0ec76a9f01e3281dab166992b4129239c7a7
walter <walter52@sohu.com>
2024-07-28 11:13:59 +0800

    更新忽略项

--- led.py
+++ led.py
@@ -2,10 +2,16 @@ import machine
 import time
 
 
-pin2 = machine.Pin(2, machine.Pin.OUT)
 
+pin2 = machine.Pin(2, machine.Pin.OUT)
+#
 while True:
     pin2.value(1)
     time.sleep(1)
     pin2.value(0)
     time.sleep(1)
+
+#
+# print(pin2.value())
+# pin2.value(1)
+# print(pin2.value())
